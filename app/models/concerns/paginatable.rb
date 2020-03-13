057787d9a8ea239b6dd5e8dcb809c94f70c828ed
Dmitry Jum <xfiler@ymail.com>
2020-03-13 18:29:05 -0400

    adds controller, paginatable concern, inits specs, adds routes

---
+++ app/models/concerns/paginatable.rb
@@ -0,0 +1,18 @@
+module Paginatable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :paginate, -> (params = {}) do
+      response = {records: self, entries_count: self.count, pages_per_limit: 1, page: 1}
+      unless params[:per_page].nil? && params[:page].nil?
+        per_page = params[:per_page].nil? ? 9 : params[:per_page].to_i
+        response[:page] = params[:page].nil? ? 1 : params[:page].to_i
+        response[:pages_per_limit] = response[:entries_count] / per_page + 1
+        response[:records] = response[:entries_count] <= per_page ? self.all.order('ASC') :
+                  self.offset((response[:page] - 1) * per_page).order('ASC').limit(per_page)
+        response
+      end
+      response
+    end
+  end
+end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,5 @@
 class Image < ApplicationRecord
+  include Paginatable
+  validates_presence_of :url
+  validates_uniqueness_of :url
 end
