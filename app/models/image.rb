07c3ba033380f400ce6195534ea6b5d020faf263
Dmitry Jum <xfiler@ymail.com>
2020-03-14 18:02:47 -0400

    adds scope that searches by dimension

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,11 @@ class Image < ApplicationRecord
   include Paginatable
   validates_presence_of :url
   validates_uniqueness_of :url
+  serialize :dimensions
+
+  scope :where_dimensions_are, -> (dimensions) {where("dimensions && ?", "{#{[dimensions].flatten.join(',')}}")}
 
   def self.all_dimensions
-    Image.pluck(:dimensions).flatten.uniq
+    pluck(:dimensions).flatten.uniq
   end
 end
